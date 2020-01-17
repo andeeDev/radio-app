758da6c58fb7b079516e611ea7bbd37cbefcb7e7
AndeeDev <andrey14501450@gmail.com>
2020-01-17 16:32:55 +0200

    Created logo in appBar and animation for play button in bottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-
 import 'package:flutter/material.dart';
-
 import 'package:url_audio_stream/url_audio_stream.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -20,35 +19,64 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
-
-
-
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  __play1(){
-    AudioStream stream = new AudioStream("http://online-melodiafm.tavrmedia.ua/MelodiaFM");//http://91.121.222.81:8524/stream
-    stream.start();
-  }
-  __pause() {
 
-  }
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+
+  bool isPlaying = false;
+  AudioStream stream = new AudioStream("http://91.121.222.81:8524/stream");
+//http://online-melodiafm.tavrmedia.ua/MelodiaFM
+  __play1(){
+
+    if(isPlaying) {
+      setState(() {
+        isPlaying = false;
+      });
+
+      stream.stop();
+    } else {
+      setState(() {
+        isPlaying = true;
+      });
+      stream.start();
+    }
+
+
+    // http://91.121.222.81:8524/stream
+    // http://91.121.222.81:8524/stream
+    // http://online-melodiafm.tavrmedia.ua/MelodiaFM
+
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      appBar: AppBar(
+        title: Row(
+
+          children: [
+            Image.asset(
+              'bg/title.png',
+              fit: BoxFit.contain,
+              height: 100,
+              width: 130,
+            ),
+            Container(
+                padding: const EdgeInsets.all(8.0), child: Text('Radio Biz'))
+          ,Image.asset(
+          'bg/title2.png',
+              fit: BoxFit.contain,
+              height: 100,
+              width: 130,
+        )]
+
+        ),
+      ),
         body: new Stack(
           children: <Widget>[
             new Container(
@@ -59,54 +87,20 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             new Container(
+              padding: EdgeInsets.all(10),
               child: Align(
                 alignment: Alignment.bottomRight,
+
+
                 child: new  FloatingActionButton(
                     onPressed: __play1,
                     tooltip: 'Increment',
-                    child: Icon(Icons.play_arrow))//your elements here
+                    child: isPlaying ? Icon(Icons.pause):Icon(Icons.play_arrow) ) //your elements here
                 ),
               )]
         )
     );
   }
-
-
- /* @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: new Stack(
-        children: <Widget>[
-          new Container(decoration: new BoxDecoration(
-            image: new DecorationImage(
-                image: new AssetImage('bg/poster.jpg'),
-              fit: BoxFit.cover
-            )
-          ),),
-           new  FloatingActionButton(
-                onPressed: __play1,
-                tooltip: 'Increment',
-                child: Icon(Icons.play_arrow)
-           )
-        ],
-      )
-    );*/
-
-
-  /*@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: __play1,
-        tooltip: 'Increment',
-        child: Icon(Icons.play_arrow),
-      ),
-    );*/
-
-
 }
 
 
